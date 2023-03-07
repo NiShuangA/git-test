22765162f8aec7363dfedb4f33602601de83b27d
nishuang <1623592047@qq.com>
2023-03-07 12:33:42 +0800

    hot-fix commit2

--- src/main/java/com/atguigu/GitTest.java
+++ src/main/java/com/atguigu/GitTest.java
@@ -5,7 +5,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("hello git2");
-        System.out.println("hello git3");
-        System.out.println("hello git4");
+        System.out.println("hot test");
     }
 }
